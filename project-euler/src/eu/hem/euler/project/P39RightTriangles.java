61ce375a1f35b43895bd95ab38b3f817f43855dc
Marcell Hent <marcell.hent@dorsum.eu>
2018-10-17 20:59:17 +0200

    problem 41 solution

---
+++ project-euler/src/eu/hem/euler/project/P39RightTriangles.java
@@ -0,0 +1,31 @@
+package eu.hem.euler.project;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P39RightTriangles {
+
+	public static void main(String[] args) {
+		Map<Integer, Integer> perimeters = new HashMap<>();
+		
+		for (int c = 1; c < 668; c++) {
+			for (int b = 1; b < c; b++) {
+				for (int a = 1; a <= b && a < 334; a++) {
+					if (a + b + c <= 1000 && a * a + b * b == c * c) {
+						int p = a + b + c;
+						perimeters.put(p, perimeters.containsKey(p) ? perimeters.get(p) + 1 : 1);
+					}
+				}
+			}
+		}
+		int perimeter = 0;
+		int max = 0;
+		for (int p : perimeters.keySet()) {
+			if (perimeters.get(p) > max) {
+				perimeter = p;
+				max = perimeters.get(p);
+			}
+		}
+		System.out.println(max + " solutions for " + perimeter + " perimeter");
+	}
+}
--- project-euler/src/eu/hem/euler/project/Test.java
+++ project-euler/src/eu/hem/euler/project/Test.java
@@ -1,8 +1,13 @@
 package eu.hem.euler.project;
 
+import static eu.hem.euler.project.EulerUtils.*;
+
 public class Test {
 	
 	public static void main(String[] args) {
 
+		System.out.println(digits(5647484));
+		System.out.println(isPandigital(3125));
+		System.out.println(2 == Math.sqrt(4));
 	}
 }
