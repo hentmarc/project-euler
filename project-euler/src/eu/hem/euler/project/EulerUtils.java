0a65453585bd5a3b71452e801cee242f85999000
Marcell Hent <Marcell.Hent@dorsum.eu>
2018-10-18 12:00:18 +0200

    utils

--- project-euler/src/eu/hem/euler/project/EulerUtils.java
+++ project-euler/src/eu/hem/euler/project/EulerUtils.java
@@ -1,7 +1,5 @@
 package eu.hem.euler.project;
 
-import static java.lang.Math.sqrt;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,13 +9,13 @@ public class EulerUtils {
 
 	public static List<Integer> digits(int n) {
 		List<Integer> digits = new ArrayList<>();
-		while(n > 0) {
+		while (n > 0) {
 			digits.add(0, n % 10);
 			n /= 10;
 		}
 		return digits;
 	}
-	
+
 	public static boolean isPandigital(int n) {
 		List<Integer> digits = digits(n);
 		if (digits.size() > 9) {
@@ -26,24 +24,5 @@ public class EulerUtils {
 		List<Integer> sample = IntStream.rangeClosed(1, digits.size()).boxed().collect(Collectors.toList());
 		digits.sort(null);
 		return sample.equals(digits);
-	}
-	
-	public static boolean isPrime(int n) {
-		if (n < 2) {
-			return false;
-		} else if (n < 4) {
-			return true;
-		} else if (n % 2 == 0 || n % 3 == 0) {
-			return false;
-		} else if (n < 8) {
-			return true;
-		} else {
-			for (int i = 5; i <= sqrt(n); i += 6) {
-				if (n % i == 0 || n % (i + 2) == 0) {
-					return false;
-				}
-			}
-		}
-		return true;
 	}
 }
--- project-euler/src/eu/hem/euler/project/P41PandigitalPrime.java
+++ project-euler/src/eu/hem/euler/project/P41PandigitalPrime.java
@@ -1,20 +1,25 @@
 package eu.hem.euler.project;
 
-import static eu.hem.euler.project.EulerUtils.*;
+import static eu.hem.euler.project.EulerUtils.isPandigital;
+import static eu.hem.euler.project.PrimeUtils.isPrime;
+import static eu.hem.euler.project.ProcessUtils.printDuration;
 
 public class P41PandigitalPrime {
 
 	public static void main(String[] args) {
-		int max = 0;
-		for (int i = 5; i <= 7654321; i += 6) {
-			if (isPandigital(i) && isPrime(i) && i > max) {
-				max = i;
-			}
-			if (isPandigital(i + 2) && isPrime(i + 2) && i + 2 > max) {
-				max = i + 2;
-			}
-		}
-		System.out.println(max);
+		System.out.println(maxPandigitalPrime());
+		printDuration();
 	}
 
+	public static int maxPandigitalPrime() {
+		for (int i = 7654321; i > 1234; i -= 6) {
+			if (isPandigital(i) && isPrime(i)) {
+				return i;
+			}
+			if (isPandigital(i - 2) && isPrime(i - 2)) {
+				return i - 2;
+			}
+		}
+		return 0;
+	}
 }
