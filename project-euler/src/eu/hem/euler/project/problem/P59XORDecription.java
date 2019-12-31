cbcde8d25b912c763885f9f9e04dd8a42a319c27
hem <hem@DIT19MPC007.dorsum.intra>
2019-12-31 11:36:53 +0100

    Problem 59

---
+++ project-euler/src/eu/hem/euler/project/problem/P59XORDecription.java
@@ -0,0 +1,16 @@
+package eu.hem.euler.project.problem;
+
+public class P59XORDecription {
+
+	public static void main(String[] args) {
+		System.out.println(Integer.toBinaryString(65));
+		System.out.println(Integer.toBinaryString(42));
+		System.out.println(65 ^ 42);
+		
+		System.out.println(0b1101011);
+		
+		char c = ' ';
+		System.out.println(c - 0);
+	}
+
+}
