d4dc1f5ef255df1b7bebbee00610deb520fe13e6
hem <hem@DIT19MPC007.dorsum.intra>
2020-10-31 12:59:48 +0100

    Problem 62 solution

--- project-euler/src/eu/hem/euler/project/solution/P62CubicPermutatitons.java
+++ project-euler/src/eu/hem/euler/project/solution/P62CubicPermutatitons.java
@@ -1,8 +1,8 @@
 package eu.hem.euler.project.solution;
 
-import static eu.hem.euler.project.util.DigitUtils.digits;
 import static eu.hem.euler.project.util.ProcessUtils.printDuration;
 import static java.lang.Math.pow;
+import static java.util.Arrays.sort;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,25 +13,24 @@ public class P62CubicPermutatitons {
 
 	public static void main(String[] args) {
 
-		Map<List<Integer>, List<Integer>> cubes = new HashMap<>();
+		Map<String, List<Integer>> cubes = new HashMap<>();
 
 		for (int i = 1; i < 10_000; i++) {
-			List<Integer> digits = digits((long) pow(i, 3));
-			digits.sort(null);
-
-			if (!cubes.containsKey(digits)) {
-				List<Integer> cbrts = new ArrayList<>();
-				cbrts.add(i);
-				cubes.put(digits, cbrts);
-			} else {
-				List<Integer> cbrts = cubes.get(digits);
-				cbrts.add(i);
-				if (cbrts.size() == 5) {
-					System.out.println((long) pow(cbrts.get(0), 3));
-					printDuration();
-					return;
-				}
+			String key = generateKey((long) pow(i, 3));
+			List<Integer> cbrts = cubes.containsKey(key) ? cubes.get(key) : new ArrayList<>();
+			cbrts.add(i);
+			cubes.put(key, cbrts);
+			if (cbrts.size() == 5) {
+				System.out.println((long) pow(cbrts.get(0), 3));
+				printDuration();
+				return;
 			}
 		}
+	}
+
+	public static String generateKey(long l) {
+		char[] chars = String.valueOf(l).toCharArray();
+		sort(chars);
+		return String.valueOf(chars);
 	}
 }
