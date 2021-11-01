bb26b857712a8224af474fb392b0d146a0e9fe3a
DORSUM\hem <hem@dorsum.eu>
2021-11-01 09:45:11 +0100

    Problem 87 solution

--- project-euler/src/eu/hem/euler/project/solution/P1Multiples3n5.java
+++ project-euler/src/eu/hem/euler/project/solution/P1Multiples3n5.java
@@ -1,15 +1,12 @@
 package eu.hem.euler.project.solution;
 
-import static eu.hem.euler.project.util.ProcessUtils.printDuration;
-
 public class P1Multiples3n5 {
 
 	public static void main(String[] args) {
-		int i = (999 / 3) * (3 + 999) / 2;
-		int j = (995 / 5) * (5 + 995) / 2;
-		int k = (990 / 15) * (15 + 990) / 2;
-
-		System.out.println(i + j - k);
-		printDuration();
+		System.out.println(
+			333 * (3 + 999) / 2 +
+			199 * (5 + 995) / 2 -
+			66 * (15 + 990) / 2
+		);
 	}
 }
---
+++ project-euler/src/eu/hem/euler/project/solution/P87PrimePowerTriples.java
@@ -0,0 +1,36 @@
+package eu.hem.euler.project.solution;
+
+import static eu.hem.euler.project.util.PrimeUtils.primes;
+import static eu.hem.euler.project.util.ProcessUtils.printDuration;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+import java.util.BitSet;
+import java.util.HashSet;
+import java.util.Set;
+
+public class P87PrimePowerTriples {
+
+	private static final int LIMIT = (int) Math.pow(10, 7) * 5;
+
+	public static void main(String[] args) {
+		int count = 0;
+		BitSet primes = primes((int) sqrt(LIMIT));
+		Set<Integer> cache = new HashSet<>();
+
+		for (int i = primes.nextSetBit(0); pow(i, 4) < LIMIT; i = primes.nextSetBit(i + 1)) {
+			for (int j = primes.nextSetBit(0); (pow(i, 4) + pow(j, 3)) < LIMIT; j = primes.nextSetBit(j + 1)) {
+				for (int k = primes.nextSetBit(0); (pow(i, 4) + pow(j, 3) + pow(k, 2)) < LIMIT; k = primes.nextSetBit(k + 1)) {
+					int n = (int) (pow(i, 4) + pow(j, 3) + pow(k, 2));
+					if (cache.add(n)) {
+						count++;
+					}
+				}
+			}
+		}
+
+		System.out.println(count);
+		printDuration();
+	}
+
+}
