3f96089a99fece7a91fe57560f41907c192f4a45
DORSUM\hem <hem@dorsum.eu>
2021-11-22 11:17:59 +0100

    Problem 88 solution

--- project-euler/src/eu/hem/euler/project/solution/P88ProductSumNumbers.java
+++ project-euler/src/eu/hem/euler/project/solution/P88ProductSumNumbers.java
@@ -5,36 +5,26 @@ import static java.lang.Math.min;
 import static java.util.Arrays.stream;
 import static java.util.stream.IntStream.range;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class P88ProductSumNumbers {
 
-	public static final int MAX_LENGTH = 12000;
-	public static final int MAX_VALUE = MAX_LENGTH * 2;
+	public static final int N = 12000;
+	public static int iterations = 0;
 
 	public static void main(String[] args) {
-		int[] minProdSums = range(0, MAX_LENGTH + 1).map(a -> a < 2 ? 0 : 2 * a).toArray();
-
-		minProdSum(new ArrayList<Integer>(), minProdSums);
-
+		int[] minProdSums = range(0, N + 1).map(a -> a < 2 ? 0 : 2 * a).toArray();
+		minProdSum(1, 1, N, minProdSums);
 		System.out.println(stream(minProdSums).distinct().sum());
+
 		printDuration();
 	}
 
-	public static void minProdSum(List<Integer> seq, int[] minProdSums) {
-		int prod = seq.stream().reduce(1, (a, b) -> a * b);
-		int sum = seq.stream().reduce(0, (a, b) -> a + b);
-		int length = prod - sum + seq.size();
-		if (length < minProdSums.length && prod < minProdSums[length]) {
-			minProdSums[length] = prod;
+	public static void minProdSum(int prod, int n, int max, int[] minProdSums) {
+		if (n < minProdSums.length && prod < minProdSums[n]) {
+			minProdSums[n] = prod;
 		}
-		
-		int max = seq.size() == 0 ? MAX_LENGTH : min(seq.get(seq.size() - 1), MAX_VALUE / prod);
 		for (int i = 2; i <= max; i++) {
-			seq.add(i);
-			minProdSum(seq, minProdSums);
-			seq.remove(seq.size() - 1);
+			minProdSum(prod * i, n + (prod - 1) * (i - 1), min(i, (N * 2) / (prod * i)), minProdSums);
 		}
 	}
+
 }
