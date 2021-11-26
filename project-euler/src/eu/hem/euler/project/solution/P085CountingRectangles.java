6b88283f66ab0124a8b9cf9ea8c812bc2fd01439
DORSUM\hem <hem@dorsum.eu>
2021-11-26 16:02:28 +0100

    Problem 85 solution

--- project-euler/src/eu/hem/euler/project/solution/P085CountingRectangles.java
+++ project-euler/src/eu/hem/euler/project/solution/P085CountingRectangles.java
@@ -5,30 +5,23 @@ import static eu.hem.euler.project.util.ProcessUtils.printDuration;
 public class P085CountingRectangles {
 
 	public static final int TARGET = 2_000_000;
-	public static final int MAX = 100;
 	
 	public static void main(String[] args) {
-		int minDiff = TARGET;
+		int diff = TARGET;
 		int area = 0;
-		
-		for (int n = 1; n <= MAX; n++) {
-			for (int m = 1; m <= n; m++) {
-				int count = 0;
-				for (int i = 1; i <= n; i++) {
-					for (int j = 1; j <= m; j++) {
-						count += (n - i + 1) * (m - j + 1);
-					}
-				}
-				int diff = Math.abs(TARGET - count);
-				if (minDiff > diff) {
-					minDiff = diff;
-					area = n * m;
-				}
+
+		for (int n = 1; n < Math.sqrt(Math.sqrt(TARGET * 2)); n++) {
+			int m = (int) Math.sqrt(4 * TARGET / (n * (n + 1)));
+			int count = n * (n + 1) * m * (m + 1) / 4;
+			int alt = Math.abs(TARGET - count);
+			if (diff >= alt) {
+				diff = alt;
+				area = n * m;
 			}
 		}
 
 		System.out.println(area);
-		
+
 		printDuration();
 	}
 
