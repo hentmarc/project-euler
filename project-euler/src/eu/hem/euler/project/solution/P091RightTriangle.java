08d7857591214b358b1f7be3bf26a4f0fefc92b7
DORSUM\hem <hem@dorsum.eu>
2022-10-28 18:34:03 +0200

    Problem 91 solution

--- project-euler/src/eu/hem/euler/project/solution/P091RightTriangle.java
+++ project-euler/src/eu/hem/euler/project/solution/P091RightTriangle.java
@@ -1,6 +1,7 @@
 package eu.hem.euler.project.solution;
 
 import static eu.hem.euler.project.util.ProcessUtils.printDuration;
+import static java.lang.Math.min;
 
 import eu.hem.euler.project.fraction.Fraction;
 
@@ -8,20 +9,14 @@ public class P091RightTriangle {
 
 	public static void main(String[] args) {
 		final int n = 50;
-
-		// 0,0; x,0; 0,y
-		int count = 3 * n * n;
+		long count = 3 * n * n;
 
 		for (int x = 1; x <= n; x++) {
 			for (int y = 1; y <= x; y++) {
 				Fraction f = new Fraction(y, x);
-
-				for (int i = x - f.n, j = y + f.d; i >= 0 && j <= n; i -= f.n, j += f.d) {
-					count += x > y ? 2 : 1;
-				}
-				for (int i = x + f.n, j = y - f.d; i <= n && j >= 0; i += f.n, j -= f.d) {
-					count += x > y ? 2 : 1;
-				}
+				long leftCount = min(x / f.n, (n - y) / f.d);
+				long rightCount = min((n - x) / f.n, y / f.d);
+				count += (leftCount + rightCount) * (x > y ? 2 : 1);
 			}
 		}
 		System.out.println(count);
