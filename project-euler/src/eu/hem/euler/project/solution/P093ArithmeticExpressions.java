b00155dc8a0db374dc695488a162b57ac2db93d5
DORSUM\hem <hem@dorsum.eu>
2022-11-14 10:44:36 +0100

    Problem 93 solution

--- project-euler/src/eu/hem/euler/project/solution/P093ArithmeticExpressions.java
+++ project-euler/src/eu/hem/euler/project/solution/P093ArithmeticExpressions.java
@@ -1,32 +1,32 @@
 package eu.hem.euler.project.solution;
 
-import static eu.hem.euler.project.util.ProcessUtils.printDuration;
 import static eu.hem.euler.project.util.CombinatoricUtils.getCombinations;
+import static eu.hem.euler.project.util.ProcessUtils.printDuration;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class P093ArithmeticExpressions {
 	
 	public static void main(String[] args) {
-		Set<Integer> digits = Set.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
-		List<Character> operators = List.of('+', '-', '*', '/');
+		Set<Integer> digits = IntStream.range(0, 10).boxed().collect(Collectors.toSet());
+		Set<Character> operators = Set.of('+', '-', '*', '/');
 		Set<Set<Integer>> combinations = getCombinations(digits, 4);
 
 		int max = 0;
 		Set<Integer> longest = new HashSet<>();
 
 		for (Set<Integer> combination : combinations) {
-			List<Double> numbers = combination.stream().map(n -> (double) n).collect(Collectors.toList());
+			Set<Double> numbers = combination.stream().map(n -> (double) n).collect(Collectors.toSet());
 			Set<Integer> results = new HashSet<>();
 
 			createExpressions(numbers, operators, results);
-
+			
 			int consecutive = 0;
-			for (int i = 1; results.contains(i); i++) {
-				consecutive = i;
+			while (results.contains(consecutive + 1)) {
+				consecutive++;
 			}
 			if (consecutive > max) {
 				max = consecutive;
@@ -38,7 +38,7 @@ public class P093ArithmeticExpressions {
 		printDuration();
 	}
 
-	public static void createExpressions(List<Double> numbers, List<Character> operators, Set<Integer> results) {
+	public static void createExpressions(Set<Double> numbers, Set<Character> operators, Set<Integer> results) {
 		if (numbers.size() > 1) {
 			for (double n : numbers) {
 				for (double m : numbers) {
@@ -46,8 +46,8 @@ public class P093ArithmeticExpressions {
 						for (char o : operators) {
 							if (o != '/' || m != 0) {
 								double result = calculate(n, o, m);
-								List<Double> newNumbers = numbers.stream().filter(a -> a != n && a != m)
-										.collect(Collectors.toList());
+								Set<Double> newNumbers = numbers.stream().filter(a -> a != n && a != m)
+										.collect(Collectors.toSet());
 								newNumbers.add(result);
 								createExpressions(newNumbers, operators, results);
 							}
@@ -56,10 +56,12 @@ public class P093ArithmeticExpressions {
 				}
 			}
 		} else {
-			double result = numbers.get(0);
-			int intResult = (int) result;
-			if (intResult == result && result > 0) {
-				results.add(intResult);
+			double result = numbers.iterator().next();
+			if (result > 0) {
+				int intResult = (int) result;
+				if (intResult == result) {
+					results.add(intResult);
+				}
 			}
 		}
 	}
